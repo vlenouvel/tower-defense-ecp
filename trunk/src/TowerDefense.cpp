d33382e8c6d9a579726b8d63203fd4fb94c424b9
vincent.m.carrier@gmail.com <vincent.m.carrier@gmail.com@9523090e-2510-4419-0e3d-d7c7c23a2a0d>
2013-11-13 17:26:29 +0000

    testVitesseFramePerSecond

--- trunk/src/TowerDefense.cpp
+++ trunk/src/TowerDefense.cpp
@@ -2,25 +2,16 @@
 #include <exception>
 #include "App.h"
 #include <SFML/Audio.hpp>
+
 using namespace std;
+
 
 int main() {
 
+
 	// on n'aura qu'une application => creation statique et non dynamique
 	App app;
+	app.jouer();
 
-	try {
-		sf::Music music;
-		music.openFromFile("resources/sons/musicTest.ogg");
-		music.play();
-		while (app.isRunning()) {
-	   		app.boucle();
-	    }
-	} catch (const exception &e) {
-		std::cerr << "Il y a eu une erreur: " << endl << e.what();
-	}
-
-	//delete app;
-	//app = NULL;
 	return 0;
 }
